647c75d277fc0be877a0b2a5144eb235f8109496
Alejo9601 <alejandro96jm@gmail.com>
2022-09-29 19:53:53 -0300

    Added profile images

--- js/cardCarouselController.js
+++ js/cardCarouselController.js
@@ -1,28 +1,30 @@
 const realExperiences = [
   {
-    image: "",
+    image: "profile.png",
     comment:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, laboriosam et natus voluptas sed repellendus.",
   },
   {
-    image: "",
+    image: "profile2.png",
     comment:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, laboriosam et natus voluptas sed repellendus.",
   },
   {
-    image: "",
+    image: "profile.png",
     comment:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur, laboriosam et natus voluptas sed repellendus.",
   },
 ];
 
-const profileImg = document.querySelector(".rounded-container--profiles");
+const profileImg = document.querySelector(".rounded-container--profile");
 const comment = document.querySelector(".card-carousel--profiles p");
 const paginationDots = document.querySelectorAll(".card-carousel__dot");
 
 let arrayIndex = 0;
 
 setInterval(() => {
+  profileImg.style.backgroundImage = `url(public/${realExperiences[arrayIndex].image})`;
+  profileImg.style.backgroundSize = "cover";
   comment.textContent = realExperiences[arrayIndex].comment;
 
   //removing previous class asignations to dots
