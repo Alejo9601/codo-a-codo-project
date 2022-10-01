ab69c46bf654059c6945dbd2b89210ae0f73f6fa
Alejo9601 <alejandro96jm@gmail.com>
2022-10-01 13:40:38 -0300

    Added price and title from api

--- js/tabMenuController.js
+++ js/tabMenuController.js
@@ -1,30 +1,45 @@
 ////API CALL//////////////////
 
 const getAllProductsForCategory = (category) => {
-  category = encodeURI(category);
-  return fetch(`https://fakestoreapi.com/products/category/${category}`).then(
-    (response) => response.json()
-  );
+  category = escape(category);
+  console.log(category);
+  return fetch(
+    `https://fakestoreapi.com/products/category/${category}?limit=3`
+  ).then((response) => response.json());
 };
 
 //////////////////////////////
 
 const tabMenuClickables = document.querySelectorAll(".tabmenu-item__clickable");
 const productsImages = document.querySelectorAll(".product-card--sm img");
+const productsTitle = document.querySelectorAll(".product-card--sm h5");
+const productsPrices = document.querySelectorAll(".product-card--sm p i");
 
+//Paints products for a specific category
+const paintProductsForCategory = (category) => {
+  getAllProductsForCategory(category).then((apiProducts) => {
+    productsImages.forEach((productImg, index) => {
+      productImg.src = apiProducts[index].image;
+    });
+    productsTitle.forEach((productTitle, index) => {
+      productTitle.textContent = apiProducts[index].title.slice(0, 25);
+    });
+    productsPrices.forEach((productPrices, index) => {
+      productPrices.textContent = "$ " + apiProducts[index].price;
+    });
+  });
+};
+
+//First Paint
+paintProductsForCategory("electronics");
+
+//Will be executed when the user clicks on a tab element
 const handleTabClick = (ev) => {
   tabMenuClickables.forEach((clickable) => {
     clickable.parentElement.classList.remove("active");
   });
   ev.target.parentElement.classList.add("active");
-
-  getAllProductsForCategory(ev.target.textContent.toLowerCase()).then(
-    (apiProducts) => {
-      productsImages.forEach((productImg, index) => {
-        productImg.src = apiProducts[index].image;
-      });
-    }
-  );
+  paintProductsForCategory(ev.target.textContent.toLowerCase());
 };
 
 tabMenuClickables.forEach((element) => {
