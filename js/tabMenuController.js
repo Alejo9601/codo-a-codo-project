515aed4664edcaa11ce8cc351425baa2ee4f54f0
Alejo9601 <alejandro96jm@gmail.com>
2022-10-04 12:06:22 -0300

    Refactoring tabMenuController

--- js/tabMenuController.js
+++ js/tabMenuController.js
@@ -1,45 +1,78 @@
 ////API CALL//////////////////
 
-const getAllProductsForCategory = (category) => {
+const getProductsForCategory = (category, limit = 100) => {
   category = escape(category);
   console.log(category);
   return fetch(
-    `https://fakestoreapi.com/products/category/${category}?limit=3`
+    `https://fakestoreapi.com/products/category/${category}?limit=${limit}`
   ).then((response) => response.json());
 };
 
 //////////////////////////////
+const electronicsProducts = await getProductsForCategory("electronics", 3);
+const jeweleryProducts = getProductsForCategory("jewelery", 3);
+const mensClothesProducts = getProductsForCategory("men´s clothing", 3);
+const womensClothesProducts = getProductsForCategory("women´s clothing", 3);
 
 const tabMenuClickables = document.querySelectorAll(".tabmenu-item__clickable");
 const productsImages = document.querySelectorAll(".product-card--sm img");
 const productsTitle = document.querySelectorAll(".product-card--sm h5");
 const productsPrices = document.querySelectorAll(".product-card--sm p i");
 
-//Paints products for a specific category
-const paintProductsForCategory = (category) => {
-  getAllProductsForCategory(category).then((apiProducts) => {
-    productsImages.forEach((productImg, index) => {
-      productImg.src = apiProducts[index].image;
-    });
-    productsTitle.forEach((productTitle, index) => {
-      productTitle.textContent = apiProducts[index].title.slice(0, 25);
-    });
-    productsPrices.forEach((productPrices, index) => {
-      productPrices.textContent = "$ " + apiProducts[index].price;
-    });
+//Paints products
+const paintProducts = async (category) => {
+  let products;
+
+  switch (category) {
+    case "electronics":
+      products = electronicsProducts;
+      break;
+    case "jewelery":
+      products = jeweleryProducts;
+      break;
+    case "men's clothing":
+      products = mensClothesProducts;
+      break;
+    case "women's clothing":
+      products = womensClothesProducts;
+      break;
+    default:
+      break;
+  }
+
+  productsImages.forEach((productImg, index) => {
+    productImg.src = products[index].image;
+  });
+  productsTitle.forEach((productTitle, index) => {
+    productTitle.textContent = products[index].slice(0, 25);
+  });
+  productsPrices.forEach((productPrices, index) => {
+    productPrices.textContent = "$ " + products[index].price;
   });
 };
 
-//First Paint
-paintProductsForCategory("electronics");
-
-//Will be executed when the user clicks on a tab element
-const handleTabClick = (ev) => {
+const makeActiveTab = (tab) => {
   tabMenuClickables.forEach((clickable) => {
     clickable.parentElement.classList.remove("active");
   });
-  ev.target.parentElement.classList.add("active");
-  paintProductsForCategory(ev.target.textContent.toLowerCase());
+  tab.target.parentElement.classList.add("active");
+};
+
+//First Paint
+paintProducts("electronics");
+
+//Will be executed when the user clicks on a tab element
+const handleTabClick = (clickedTab) => {
+  const gridProducts = document.querySelector(".grid-products");
+  gridProducts.style.opacity = "0";
+  gridProducts.style.visibility = "hidden";
+
+  makeActiveTab(clickedTab);
+
+  paintProducts(clickedTab.target.textContent.toLowerCase()).then(() => {
+    gridProducts.style.opacity = "1";
+    gridProducts.style.visibility = "visible";
+  });
 };
 
 tabMenuClickables.forEach((element) => {
