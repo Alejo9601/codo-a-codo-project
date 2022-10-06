0f3a890bd9dd85d2dd38ca17cf23871234fba0b8
Alejo9601 <alejandro96jm@gmail.com>
2022-10-05 22:21:12 -0300

    Still not working responsive menu

--- js/menuTogglerController.js
+++ js/menuTogglerController.js
@@ -1,17 +1,24 @@
 const menuToggler = document.querySelector(".menu-toggler");
-const menu = document.querySelector(".menu-list--header");
+const menu = document.querySelector(".menu");
 
 const handleMenuToggler = (ev) => {
-  if (menu.classList.contains("dropped")) {
-    menu.style.display = "none";
-    menu.classList.remove("dropped");
+  if (menu.classList.contains("menu--active")) {
+    menu.classList.remove("menu--active");
+    menu.classList.add("menu--inactive");
     return;
   }
-  menu.style.display = "flex";
-  menu.classList.add("dropped");
+  menu.classList.add("menu--active");
+  menu.classList.remove("menu--inactive");
 };
 
 const setMenuToglerController = () => {
+  window.addEventListener("resize", () => {
+    if (window.innerWidth() <= 580) {
+      menu.classList.add("menu--inactive");
+      return;
+    }
+    menu.classList.remove("menu--inactive");
+  });
   menuToggler.addEventListener("click", handleMenuToggler);
 };
 
