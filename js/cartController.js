b5f1822ca377526b963e396faac6dc1973b1183c
Alejo9601 <alejandro96jm@gmail.com>
2022-10-11 15:09:21 -0300

    Added functionality to cart button

--- js/cartController.js
+++ js/cartController.js
@@ -1,8 +1,17 @@
 const productCards = document.querySelectorAll(".product-card");
 const cart = document.querySelector(".cart");
+const cartDetail = document.querySelector(".cart__detail");
+const cartDetailUl = document.querySelector(".cart__detail ul");
+const buttonShowCart = document.querySelector(".btn-cart");
+
 let cartItemsCount = 0;
 
-const setProductOnCart = (productCard) => {};
+const setProductOnCart = (product) => {
+  const productDescription = product.children[1];
+  let li = document.createElement("li");
+  li.textContent = productDescription.children[0].textContent;
+  cartDetailUl.appendChild(li)
+};
 
 const incrementItemsOnCart = () => {
   cartItemsCount += 1;
@@ -21,6 +30,14 @@ const handleButtonAddToCart = (ev) => {
   incrementItemsOnCart();
 };
 
+const handleButtonShowCart = () => {
+  if(cartDetail.classList.contains("cart__detail--visible")) {
+    cartDetail.classList.remove("cart__detail--visible");
+    return;
+  }
+  cartDetail.classList.add("cart__detail--visible");
+}
+
 const setCartController = () => {
   productCards.forEach((productCard) => {
     productCard.lastElementChild.addEventListener(
@@ -28,6 +45,7 @@ const setCartController = () => {
       handleButtonAddToCart
     );
   });
+  buttonShowCart.addEventListener("click", handleButtonShowCart);
 };
 
 export default setCartController;
